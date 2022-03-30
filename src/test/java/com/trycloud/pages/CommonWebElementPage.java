3abea37307be355f25f7613dea2423692264ed7e
Ariuntuul <ariuntuulenkhtaivan@gmail.com>
2022-03-30 09:38:09 -0500

    Added 2 methods into the commonWebElementPage from basePage

--- src/test/java/com/trycloud/pages/CommonWebElementPage.java
+++ src/test/java/com/trycloud/pages/CommonWebElementPage.java
@@ -1,6 +1,9 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
@@ -70,6 +73,19 @@ public class CommonWebElementPage extends BasePage{
     @FindBy(xpath = "//input[@class='icon-confirm']")
     public WebElement iconConfirmButton;
 
+
+    public static Actions actions = new Actions(Driver.getDriver());
+
+    public static void headerModule(String head) {
+        String s = "//span[normalize-space(text())='" + head + "']";
+        actions.moveToElement(Driver.getDriver().findElement(By.xpath(s))).click().perform();
+    }
+
+
+    public static void sideModule(String moduleName) {
+        String sf = "//li[starts-with(@class,'nav')]/a[text()='" + moduleName + "']";
+        actions.moveToElement(Driver.getDriver().findElement(By.xpath(sf))).click().perform();
+    }
 
 
 }
